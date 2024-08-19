a35173f19a3b7466c1c3696a809f381bc6d3f4fd
kuldeep-bst <97655816+kuldeep-bst@users.noreply.github.com>
2024-08-19 20:47:56 +0530

    second commit

--- sampleData.js
+++ sampleData.js
@@ -1,4 +1,4 @@
-export default sampleData=[
+export default [
     {id: crypto.randomUUID(),title: 'Milk',category: 'Grocery',amount: 40},
     {id: crypto.randomUUID(),title: 'Shirt',category: 'Clothes',amount: 600},
     {id: crypto.randomUUID(),title: 'Vegitable',category: 'Grocery',amount: 100},
